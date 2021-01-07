6b94d8c561cc72fdb8d2ecb2ef15748fae236c1b
ThayanaRMachado <thayanarm@hotmail.com>
2021-01-07 12:06:18 -0300

    testando o envio para produção

--- src/main/java/com/example/algamoneyapi1/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/example/algamoneyapi1/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -19,10 +19,6 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
